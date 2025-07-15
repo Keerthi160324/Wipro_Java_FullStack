173b8898adc5e5be2b486e626621f0139146b70e
Keerthi Madugula <keerthireddymadugula16@gmail.com>
2025-07-15 14:15:41 +0530

    Create lessthanorgreaterthan.java

---
+++ lessthanorgreaterthan.java
@@ -0,0 +1,14 @@
+class Main{
+public static void main(String[]args){
+int a=5;
+if(a==5){
+System.out.println("It is 5");
+}
+else if(a<5){
+System.out.println("It is less than 5");
+}
+else{
+System.out.println("It is greater than 5");
+}
+}
+}
