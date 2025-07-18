34f4e4a11c188dca906d32baa59ae5c0e91bdf09
Keerthi Madugula <keerthireddymadugula16@gmail.com>
2025-07-18 11:52:46 +0530

    Create Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,27 @@
+package Wrapper;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		System.out.println("Interger Value");
+		System.out.println("min:"+Integer.MIN_VALUE);
+		System.out.println("max:"+Integer.MAX_VALUE);
+		
+		System.out.println("Double Value");
+		System.out.println("min:"+Double.MIN_VALUE);
+		System.out.println("max:"+Double.MAX_VALUE);
+		
+		System.out.println("Float Value");
+		System.out.println("min:"+Float.MIN_VALUE);
+		System.out.println("max:"+Float.MAX_VALUE);
+		
+		System.out.println("Byte Value");
+		System.out.println("min:"+Byte.MIN_VALUE);
+		System.out.println("max:"+Byte.MAX_VALUE);
+		
+		System.out.println("Short Value");
+		System.out.println("min:"+Short.MIN_VALUE);
+		System.out.println("max:"+Short.MAX_VALUE);
+	}
+
+}
