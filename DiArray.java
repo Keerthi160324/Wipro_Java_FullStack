270103712d354870216adc59625a8b11bc462c7a
Keerthi Madugula <keerthireddymadugula16@gmail.com>
2025-07-18 10:06:46 +0530

    Create DiArray.java

---
+++ DiArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class DiArray{
+public static void main(String[]args){
+int[]x;
+x=new int[5];
+x[0]=07;
+x[1]=25;
+x[2]=38;
+for(int i=0;i<=x.length-1;i++){
+System.out.println("Element at index:"+x[i]);
+}
+}
+}
